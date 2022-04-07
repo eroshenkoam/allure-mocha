be98193f40989fa71843a6d4389976aaede9d70d
Artem Eroshenko <eroshenkoam@qameta.io>
2022-04-07 20:43:28 +0300

    fix node version

--- specs/simple.spec.js
+++ specs/simple.spec.js
@@ -1,15 +1,14 @@
 const AllureRuntime = require("allure-mocha/runtime");
 
 describe("This is suite", () => {
-
-  AllureRuntime.allure.feature("This is feature") 
-  AllureRuntime.allure.story("This is story")
   it("some first second spec", () => {
+    AllureRuntime.allure.feature("This is feature");
+    AllureRuntime.allure.story("This is story");
     AllureRuntime.allure.step("Open main page", () => {
-        // browser.url("https://www.google.com");
+      // browser.url("https://www.google.com");
     });
     AllureRuntime.allure.step("Go to repository", () => {
-        // ... next steps
-    })
+      // ... next steps
+    });
   });
 });
