56ca3a160d479e0ed1c3962b3c0bbd854cb7260b
Artem Eroshenko <eroshenkoam@qameta.io>
2022-04-07 20:40:28 +0300

    add more steps

--- specs/simple.spec.js
+++ specs/simple.spec.js
@@ -1,10 +1,15 @@
 const AllureRuntime = require("allure-mocha/runtime");
 
-describe("Allure", () => {
-  it("some first simple spec", () => {});
-  describe("dipper test", () => {
-    it("some first second spec", () => {
-      AllureRuntime.allure.step("demo step created");
+describe("This is suite", () => {
+
+  AllureRuntime.allure.feature("This is feature") 
+  AllureRuntime.allure.story("This is story")
+  it("some first second spec", () => {
+    AllureRuntime.allure.step("Open main page", () => {
+        // browser.url("https://www.google.com");
     });
+    AllureRuntime.allure.step("Go to repository", () => {
+        // ... next steps
+    })
   });
 });
