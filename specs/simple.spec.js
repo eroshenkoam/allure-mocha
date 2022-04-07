0995d11caee170a39af9d05f42accc5c74c0b017
Artem Eroshenko <eroshenkoam@qameta.io>
2022-04-07 12:27:15 +0300

    add report config

--- specs/simple.spec.js
+++ specs/simple.spec.js
@@ -1,6 +1,10 @@
-describe('Allure', () => {
-    it('some first simple spec', () => {});
-    describe("dipper test", () => {
-        it('some first second spec', () => {});
-    })
+const { allure } = require("allure-mocha/runtime");
+
+describe("Allure", () => {
+  it("some first simple spec", () => {});
+  describe("dipper test", () => {
+    it("some first second spec", () => {
+        console.log(allure)
+    });
+  });
 });
